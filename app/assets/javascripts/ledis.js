6b6165554b3efaed9dfbc9d7e59336d38b915a11
Minh Phan <pbminh2511@gmail.com>
2020-04-18 18:14:21 +0700

    Revert "Revert "Refactoring and adding a bit of UI""
    This reverts commit 634e5717e52f370182cc7e4ed7c1f3a4acd18c23.

--- app/assets/javascripts/ledis.js
+++ app/assets/javascripts/ledis.js
@@ -1,24 +1,30 @@
 $(document).on("turbolinks:load", () => {
-    let input_history = [""];
-    let current_input = 0;
+    function focus_input(){
+        $("#terminal").focus().val(">");
+    }
     
     function display(input, is_result = false) {
         let result = $("#result");
         if (!is_result){
-            result.append(">\t" + input + "<br>");
+            result.append("<div class='input'>>\t" + input + "</div>");
         } else {
-            result.append(input + "<br>");
+            result.append("<div>\t" + input + "</div>");
         }
     }
+
+    let input_history = [];
+    let current_input = -1;
+    focus_input();
 
     $("#terminal").on("keydown", (event) => {
         let input = $("#terminal");
         if (event.which == 13){
+            let command = input.val().slice(1);
             $.ajax({
                 url: "parse",
                 type: "post",
                 data: {
-                    command: input.val()
+                    command: command
                 },
                 dataType: "json",
                 success: (data, status, xhr) => {
@@ -26,25 +32,35 @@ $(document).on("turbolinks:load", () => {
                 }
             });
 
-            input_history.push(input.val());
+            input_history.push(command);
             current_input = input_history.length;
 
-            display(input.val());
-            input.val("");
+            display(command);
+            focus_input();
         }
     })
 
     $("#terminal").on("keydown", (event) => {
         let input = $("#terminal");
         if (input_history.length > 0){
+            
             if (event.which == 38){
-                (current_input > 0) ? current_input -= 1 : current_input = input_history.length - 1
-                input.val(input_history[current_input]);
+                (current_input > 0) ? current_input -= 1 : ""
+                input.val(">" + input_history[current_input]).focus();
+                event.preventDefault();
             } else if (event.which == 40){
-                (current_input < input_history.length - 1) ? current_input += 1 : ""
-                input.val(input_history[current_input]);
+                if (current_input < input_history.length - 1){
+                    current_input += 1
+                    input.val(">" + input_history[current_input]);
+                } else {
+                    if (current_input < input_history.length) current_input += 1;
+                    focus_input();
+                }
             }
             
+        }
+        if (event.which == 8 && input.val().length == 1){
+            event.preventDefault();
         }
     })
 })
--- app/services/response_handler.rb
+++ app/services/response_handler.rb
@@ -1,45 +1,50 @@
 class ResponseHandler
-    attr_reader :result, :type
-    def initialize(result:, type:)
-        @result = result
-        @type = type
-    end
+  attr_reader :result, :type
+  def initialize(result:, type:)
+    @result = result
+    @type = type
+  end
 
-    def perform
-        send(handler(type))
+  def perform
+    response = send(handler(type))
+    if response.class == Integer
+        response = "(integer) #{response}"
     end
+    response
+  end
 
-    private
-    def handler(type)
-        (type.downcase.to_s + "_handler").to_sym
-    end
+  private
 
-    def error_handler
-        "<div class=\"error\">\t(error) #{result.class.to_s.demodulize} #{result.message} </div>"
-    end
+  def handler(type)
+    (type.downcase.to_s + "_handler").to_sym
+  end
 
-    def basic_handler
-        result
-    end
+  def error_handler
+    "<div class=\"error\">\t(error) #{result.class.to_s.demodulize} #{result.message} </div>"
+  end
 
-    def count_handler
-        result
-    end
+  def basic_handler
+    result
+  end
 
-    def list_handler
-        return "(empty list or set)" if result.empty?
-        response = ""
-        result.each_with_index do |k, i|
-            response += "#{i + 1}) #{k}<br>"
-        end
-        response[0..-5]
-    end
+  def count_handler
+    result
+  end
 
-    def bool_handler
-        result ? 1 : 0
+  def list_handler
+    return "(empty list or set)" if result.empty?
+    response = ""
+    result.each_with_index do |k, i|
+        response += "#{i + 1}) \"#{k}\"<br>"
     end
+    response[0..-5]
+  end
 
-    def value_handler
-        result
-    end
+  def bool_handler
+    response = result ? 1 : 0
+  end
+
+  def value_handler
+    "\"#{result}\""
+  end
 end
